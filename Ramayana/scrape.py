fb30bb756f941530d20143ad25b4daee7bc77abf
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-06-25 19:19:42 +0530

    final solution each Kanda has a one shloka

--- Ramayana/scrape.py
+++ Ramayana/scrape.py
@@ -39,12 +39,10 @@ def Parse(value1, value2, value3):
     except IndexError:
         return None, None
 
-
     sanskrit_shloka = ''
     for i in allX:
         if isEng(i):
             continue
-
 
         first = str(i.encode('utf-8'))
         for aa in range(-7, 8):
@@ -52,12 +50,12 @@ def Parse(value1, value2, value3):
                 first = first.replace(str(value1) + '.' + str(value2 + aa) + '.' + str(value3 + bb), '')
                 first = first.replace(str(value1) + str(value2 + aa) + str(value3 + bb), '')
 
-        first = first.replace('\xe0\xa5\xa4', '')   # |
+        first = first.replace('\xe0\xa5\xa4', '')  # |
 
         if i == allX[-1]:
-            first += '\xe0\xa5\xa4\xe0\xa5\xa4'     # ||
+            first += '\xe0\xa5\xa4\xe0\xa5\xa4'  # ||
         else:
-            first += '\xe0\xa5\xa4'                 # |
+            first += '\xe0\xa5\xa4'  # |
 
         sanskrit_shloka += first + ' '
 
@@ -69,8 +67,16 @@ def Parse(value1, value2, value3):
     except ExceptionGroup:
         return None, None
 
-    print(sanskrit_shloka)
-    print(english_shloka)
+    sanskrit_shloka = sanskrit_shloka.strip()
+    sanskrit_shloka = sanskrit_shloka.strip(' ')
+    sanskrit_shloka = sanskrit_shloka.strip('\n')
+    english_shloka = english_shloka.strip()
+    english_shloka = english_shloka.strip('\n')
+
+    if english_shloka == '':
+        return None, None
+
+    return sanskrit_shloka, english_shloka
 
 
 def get(kanda, sarga, kanda_tid):
@@ -78,17 +84,46 @@ def get(kanda, sarga, kanda_tid):
     for sarga_n in range(1, sarga):
         count = 0
         for sloka_n in range(1, 2):
-            # sanskrit, english = Parse(kanda_tid, sarga_n, sloka_n)
-            Parse(kanda_tid, sarga_n, sloka_n)
+            sanskrit, english = Parse(kanda_tid, sarga_n, sloka_n)
+
+            if sanskrit is None and english is None:
+                count += 1
+                if count >= 20:
+                    break
+            else:
+                count = 0
+
+            if (sanskrit, english) in Member:
+                continue
+            else:
+                Member.append((sanskrit, english))
+
+            if len(Member) >= 25:
+                Member = Member[1:]
+
+            print(sloka_n)
+            if sanskrit is not None and english is not None:
+                count = 0
+                with open(str(kanda) + '_sanskrit.txt', 'a+', encoding='utf-8') as file:
+                    file.write(sanskrit + '\n')
+                with open(str(kanda) + '_english.txt', 'a+', encoding='utf-8') as file:
+                    file.write(english + '\n')
+
+                file.close()
+            else:
+                count += 1
+                if count >= 20:
+                    break
 
 
 # Content: name(kanda), sarga, Sloka
-# Kandas = [('balakanda', 78, 1), ('ayodhyakanda', 120, 2), ('aranyakanda', 76, 3), ('kishkindakanda', 68, 4),
-#           ('sundarkanda', 69, 5), ('yuddhakanda', 132, 6)]
+Kandas = [('balakanda', 78, 1), ('ayodhyakanda', 120, 2), ('aranyakanda', 76, 3), ('kishkindakanda', 68, 4),
+          ('sundarkanda', 69, 5), ('yuddhakanda', 132, 6)]
 
 # Example
-Kandas = [('balakanda', 2, 1), ('ayodhyakanda', 2, 2), ('aranyakanda', 2, 3), ('kishkindakanda', 2, 4),
-          ('sundarkanda', 2, 5), ('yuddhakanda', 2, 6)]
+# Kandas = [('balakanda', 2, 1), ('ayodhyakanda', 2, 2), ('aranyakanda', 2, 3), ('kishkindakanda', 2, 4),
+#           ('sundarkanda', 2, 5), ('yuddhakanda', 2, 6)]
+
 
 for kanda_no, sarga_no, kanda_id in Kandas:
     get(kanda_no, sarga_no, kanda_id)
