12a85e8508535a1802cefe2181c36df031102da1
KULDEEP DHANGAR <kuldeepdhangarkd@gmail.com>
2023-10-09 16:17:22 +0530

    try to catching

--- ramcharitmanas/scraper.py
+++ ramcharitmanas/scraper.py
@@ -4,4 +4,15 @@ import os
 import dotenv
 dotenv.load_dotenv()
 URL = os.getenv("URL")
-print(URL)
+while True:
+    try:
+        html_content = urllib.request.urlopen(URL).read()
+        break
+    except ExceptionGroup:
+        pass
+
+html_string = html_content.decode(errors="ignore")
+html_content = html_string.replace('\r', '')
+soup = bs4.BeautifulSoup(html_content, 'html.parser')
+filed = soup.findAll('div', {'class': 'field-content'})
+print(filed)
